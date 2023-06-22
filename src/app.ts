ea7107622555074f4a50d854e2b76797be6640dc
vikas-sumfactor <vikas.sumfactor@gmail.com>
2023-06-22 15:46:46 +0530

    upload file

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,20 @@ import * as url from "url";
 import EventEmitter from "events";
 import { log } from "console";
 
+ import multer from "multer";
+
+import nodemailer from "nodemailer"
+import express , { NextFunction ,Response} from "express";
+
+
+
 const port = 7700
 
-const server = createServer((request: IncomingMessage, response: ServerResponse) => {
+const app = express();
+
+
+
+// const server = createServer((request: IncomingMessage, response: ServerResponse) => {
 
 
     // if(request.url=="/name"){
@@ -20,65 +31,94 @@ const server = createServer((request: IncomingMessage, response: ServerResponse)
     //     }
     // }
 
-    if (request.url == "/name") {
-        const dir = fs.readdirSync(__dirname)
+//     if (request.url == "/name") {
+//         const dir = fs.readdirSync(__dirname)
   
-        //  file creation 
-        const writeFile = fs.writeFile("myFile.txt", "file created", function (err) {
-           if (err) throw err;
-            console.log("File written Sucessfully ")})}
+//         //  file creation 
+//         const writeFile = fs.writeFile("myFile.txt", "file created", function (err) {
+//            if (err) throw err;
+//             console.log("File written Sucessfully ")})}
  
-        // reading of file
+//         // reading of file
 
-        const readFile = fs.readFileSync(path.join(__dirname, 'myFile.txt'), { encoding: 'utf-8' })
-          console.log(readFile)
+//         const readFile = fs.readFileSync(path.join(__dirname, 'myFile.txt'), { encoding: 'utf-8' })
+//           console.log(readFile)
 
- // for new file creation
+//  // for new file creation
 
-           const appndFile = fs.appendFile('mynewfile1.txt', 'i am new file', function (err) {
-            if (err) throw err;
-            console.log('new file created by append');
-        });
+//            const appndFile = fs.appendFile('mynewfile1.txt', 'i am new file', function (err) {
+//             if (err) throw err;
+//             console.log('new file created by append');
+//         });
 
        
-        // for rename of filename
+//         // for rename of filename
 
-        const renamFile = fs.rename('myFile.txt', 'myrenamedfile.txt', function (err) {
-            if (err) throw err;
-            console.log(' my File Renamed!');
-        });
+//         const renamFile = fs.rename('myFile.txt', 'myrenamedfile.txt', function (err) {
+//             if (err) throw err;
+//             console.log(' my File Renamed!');
+//         });
 
-// for deletion
+// // for deletion
 
-        const deletedFile = fs.unlink('mynewfile1.txt', function (err) {
-            if (err) throw err;
-            console.log('my File is deleted!');
-          });
-          var adr = 'http://localhost:8080/default.htm?year=2023&month=april&name=vikasrana&tech=fullstack';
+//         const deletedFile = fs.unlink('mynewfile1.txt', function (err) {
+//             if (err) throw err;
+//             console.log('my File is deleted!');
+//           });
+//           var adr = 'http://localhost:8080/default.htm?year=2023&month=april&name=vikasrana&tech=fullstack';
         
-          var q = url.parse(adr, true);
+//           var q = url.parse(adr, true);
   
-          console.log(q)
+//           console.log(q)
   
 
-      }
-)
+//       }
+// )
 
-server.listen(port, () => console.log(`server islistining at port ${port}`))
+// server.listen(port, () => console.log(`server islistining at port ${port}`))
 
-  const event = new EventEmitter();
+//   const event = new EventEmitter();
 
-  event.on('event-1' ,() => {
-    console.log("event 1 is firedd")
-  })
+//   event.on('event-1' ,() => {
+//     console.log("event 1 is firedd")
+//   })
  
-  event.on("event-2", (arr1,arr2) => {
-    console.log("event 2 is fired ${arr1} , ${arr2}")
-  })
+//   event.on("event-2", (arr1,arr2) => {
+//     console.log("event 2 is fired ${arr1} , ${arr2}")
+//   })
 
-  event.emit("event-1");
+//   event.emit("event-1");
 
-  event.emit("event-2" , "hello","hi")
+//   event.emit("event-2" , "hello","hi")
 
 
+
+
+//   const storage = multer.diskStorage({
+//     destination(req, file, callback) {
+//         callback(null, './src')
+//     },
+//     filename(req, file, callback) {
+//         callback(null, file.originalname)
+//     },
+// })
+
+
+// const upload = multer({ storage: storage })
+
+
+
+
+
+// app.get('/uploadFile', upload.single('data'), (req: Request, res: Response, next: NextFunction) => {
+
+//     try {
+//         const file = req.file;
+//         console.log( file)
+//         res.status(200).send("file is sucessfully saved")
+//     } catch (error) {
+//         console.log( error)
+//     }
+// })
+// app.listen(7700);
 
